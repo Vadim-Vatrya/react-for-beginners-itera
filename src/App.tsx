49a99f0fab2bb1536435fdb1c5bb38c21d912831
Vadim Vatrya <VatryaVV@gmail.com>
2022-05-04 16:08:29 +0300

    add new app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MyCard from './components/MyCard';
+
+function App() {
+  return (
+    <div>
+     <MyCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyCard/MyCard.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import data from '../data/data.json';
+
+import styles from './MyCard.module.scss';
+
+const MyCard: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <div>
+                <h1 className={styles.main}>{data.surname}</h1>
+            </div>
+            <div>
+            
+                <h2 className={styles.summary}>Summary</h2>
+                <p className={styles.text}>{data.summary}</p>
+            </div>
+            <div>
+                <h2 className={styles.summary}>Contacts</h2>
+                <div className={styles.contact}>
+                         <a className={styles.link} href={data.link1} >
+        {data.email}
+                </a>
+                 <a className={styles.link} href={data.link2} >
+        {data.telephone}
+                </a>
+                 <a className={styles.link} href={data.link3} >
+        {data.telegram}
+      </a>
+            
+                </div>    
+            </div>
+        </div>
+    );
+};
+
+export default MyCard;
